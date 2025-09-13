de4883ccef783570297465b448e5f3622d7fe913
ajaySingh2615 <ajaysingh261526@gmail.com>
2025-09-14 02:17:06 +0530

    done with base entity and category model

--- src/main/java/com/cadt/blog/domain/Category.java
+++ src/main/java/com/cadt/blog/domain/Category.java
@@ -1,5 +1,33 @@
 package com.cadt.blog.domain;
 
-public class Category {
+import com.cadt.blog.common.entity.BaseEntity;
+import io.micrometer.core.annotation.Counted;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "categories",
+        uniqueConstraints = {
+                @UniqueConstraint(name = "uk_category_name", columnNames = "name"),
+                @UniqueConstraint(name = "uk_category_slug", columnNames = "slug")
+        },
+        indexes = {
+                @Index(name = "idx_category_slug", columnList = "slug")
+        })
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Category extends BaseEntity {
+
+    @Column(nullable = false, length = 120)
+    private String name;
+
+    @Column(nullable = false, length = 160)
+    private String slug;
+
+    @Column(columnDefinition = "text")
+    private String description;
 
 }
