97f9cc20d789dd294392373d7114b673e6a4ceef
ajaySingh2615 <ajaysingh261526@gmail.com>
2025-09-14 01:37:39 +0530

    done with post status and base entity files

--- src/main/java/com/cadt/blog/common/entity/BaseEntity.java
+++ src/main/java/com/cadt/blog/common/entity/BaseEntity.java
@@ -1,5 +1,36 @@
 package com.cadt.blog.common.entity;
 
-public class BaseEntity {
+import java.time.Instant;
 
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import jakarta.annotation.Generated;
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import lombok.Setter;
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+@Getter
+@Setter
+public abstract class BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @CreatedDate
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private Instant createdAt;
+
+    @LastModifiedDate
+    @Column(name = "updated_at", nullable = false)
+    private Instant updatedAt;
 }
---
+++ src/main/java/com/cadt/blog/common/enums/PostStatus.java
@@ -0,0 +1,6 @@
+package com.cadt.blog.common.enums;
+
+public enum PostStatus {
+    DRAFT,
+    PUBLISHED
+}
